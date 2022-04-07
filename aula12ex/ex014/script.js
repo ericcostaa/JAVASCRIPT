75a0b7fa8fd53c66490f288df15cae10610dab83
Eric Costa <ericcosta99@outlook.com>
2022-04-07 16:46:27 -0300

    desafio

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,27 @@
-alert('olá')
+function carregar () {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+
+msg.innerHTML = `<p>Agora são ${hora} horas.<p>`
+
+    if (hora >= 0 && hora < 12) {
+    //bom dia
+         img.src = 'foto-manha.png'
+         document.body.style.background = '#ecddbc'
+    }
+
+    else if ( hora >= 12 && hora <= 18) {
+        // boa tarde
+        img.src = 'foto-tarde.png'
+        document.body.style.background = '#DA8A77'
+    }
+
+    else  {
+        img.src = 'foto-noite.png'
+        document.body.style.background = '#0F2131'
+        //boa noite
+    }
+}
