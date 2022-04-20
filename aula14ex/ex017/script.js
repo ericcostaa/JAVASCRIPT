f5172a28b5d26a532d6e6f45c85236cbf6dea955
Eric Costa <ericcosta99@outlook.com>
2022-04-19 21:23:52 -0300

    aula14ex02

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,39 @@
-alert('olá')
+function calcular () {
+
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número')
+    } 
+    else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        
+       /* while ( c <= 10) {
+            let iten = document.createElement('option')
+            iten.text= `${n} x ${c} = ${n*c}`
+            tab.appendChild(iten)
+            c++ 
+        }*/
+
+        for(let c = 1; c<=10; c++){
+
+            let iten = document.createElement('option')
+            iten.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(iten)
+            iten.value = `tab ${c}`
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+}
