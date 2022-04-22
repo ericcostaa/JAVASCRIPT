3b2bc70be671107243062e2072ee395808c8ded3
Eric Costa <ericcosta99@outlook.com>
2022-04-22 20:11:16 -0300

    att aula15

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -5,10 +5,10 @@ console.log(num)
 console.log(`O vetor tem ${num.length} elementos`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-let pos = num.indexOf(3)
+let pos = num.indexOf(9)
 
 if (pos == -1) { 
     console.log('O valor não foi encontrado')
 }
 else{
-console.log(`O valor 8 está na posição ${pos}`)}
+console.log(`O valor está na posição ${pos}`)}
