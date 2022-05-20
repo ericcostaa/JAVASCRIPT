a26d34ac0c72796431472b10a7249304a4976564
Eric Costa <ericcosta99@outlook.com>
2022-05-20 20:47:27 -0300

    dsf

--- praticando/pratica03/script.js
+++ praticando/pratica03/script.js
@@ -1,13 +1,25 @@
-let numero = document.getElementById ("num");
-let enviar = document.querySelector ("botao");
-let resultado = document.getElementById("res")
+let NumAle = Math.floor(Math.random() * 100) + 1;
+let palpite = querySelector('.palpites');
+let UltimoRes = querySelector('.UltimoRes');
+let BaixoOuAlto= querySelector('.BaixoOuAlto');
 
-function enviar() {
-    if (numero.value > num) {
-        alert ("Número muito baixo")
-    } else if ( numero.value < num) {
-        alert (" Número muito alto")
-    } else {
-        alert( "Você acertou!")
+let botao = querySelector('.botao');
+let num = querySelector('.num');
+
+let contagem = 1;
+let reinicio;
+
+function conferirPalpite(){
+
+    let palpiteJogador = Number(palpite.value);
+    if (contagem ===1) {
+        palpites.textContent = 'Palpites anteriores: ';
     }
+    palpites.textContent += palpiteJogador + ' ';
+
+    if ( palpiteJogador === NumAle) {
+        UltimoRes.textContent = 'Parabéns você acertou!'
+        UltimoRes.style.backgroundColor = 'green';
+    }
+
 }
