aa56073d6e844248ad978d3fd2541bfdca225db4
Eric Costa <ericcosta99@outlook.com>
2022-09-12 21:32:59 -0300

    att

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,14 +1,23 @@
 let num = [5, 8, 2, 9, 3]
-num.push(1)
-num.sort()
-console.log(num)
+num.push(1) // Vai adicionar um valor no array
+num.sort() // Var botar o array em ordem crescente
+
+
+for (let indi in num ) {
+    console.log (`A posição ${indi} tem o valor ${num[indi]}`)
+}
+
+
 console.log(`O vetor tem ${num.length} elementos`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-let pos = num.indexOf(4)
+let pos = num.indexOf(3) // Vai buscar o valor () no array
 
 if (pos == -1) { 
     console.log('O valor não foi encontrado')
+        // Caso o valor não seja encontrado no vetor, ele irá me retornar o indice -1
 }
 else{
-console.log(`O valor está na posição ${pos}`)}
+ console.log(`O valor está na posição ${pos}`)}
+
+ 
--- praticando/pratica03/script.js
+++ praticando/pratica03/script.js
@@ -1,25 +1,7 @@
-let NumAle = Math.floor(Math.random() * 100) + 1;
-let palpite = querySelector('.palpites');
-let UltimoRes = querySelector('.UltimoRes');
-let BaixoOuAlto= querySelector('.BaixoOuAlto');
-
-let botao = querySelector('.botao');
-let num = querySelector('.num');
-
-let contagem = 1;
-let reinicio;
-
 function conferirPalpite(){
 
-    let palpiteJogador = Number(palpite.value);
-    if (contagem ===1) {
-        palpites.textContent = 'Palpites anteriores: ';
-    }
-    palpites.textContent += palpiteJogador + ' ';
-
-    if ( palpiteJogador === NumAle) {
-        UltimoRes.textContent = 'Parabéns você acertou!'
-        UltimoRes.style.backgroundColor = 'green';
-    }
+    let numAle = Math.floor(Math.random() * 100) + 1;
+    var palpite = document.querySelector('#num')
+    var palpeteNum = Number(palpite.value)
 
 }
